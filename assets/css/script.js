a03c13cdcc11af125424a82d8d3afbc97664a4e0
Paquito81 <hepalacios503@gmail.com>
2022-02-22 04:26:10 -0700

    added prompts questioneer

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,85 +1,47 @@
 // Assignment code here
 // The lenght of the code
-var lenghtConfirm = 8;
-var assignConfirm = [];
+var lenghtConfirm;
+var specialCharactersConfirm;
+var numberCharactersConfirm; 
+var upperCharactersConfirm; 
+var lowerCharactersConfirm; 
 
-// special characters
-var specialCharacterConfirm = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-//Number characters
-var numberCharacterConfirm = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-//Uppercase characters
-var upperCharactersConfirm = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-//Lowercase haracters
-var lowerCharactersConfirm = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-
-var generateBtn = document.querySelector("#generate");
-//Added a Greeting
+//special characters
+specialArr = ["!", "#", "$", "%", "&", "'", "(", ")", "*", ".", ",", "-", "+", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "~", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "["]
+//number characters
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+//lower case characters
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//upper case characters
+upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 //Add event listener to Generate button
-generateBtn.addEventListener("click", writePassword);
+var generateBtn = document.querySelector("#generate");
 
-   
+generateBtn.addEventListener("click", function() {
+    contra = generatePassword();
+    document.getElementById("password").placeholder = contra;
+});
 
-// 1. Prompt the user for the password criteria
-//    a. Password lenght 8 < 128
-//    b. Lowercase, uppercase, numbers ,special characters
-// 2. Validate the input.
-// 3. Display the password to the page
+//Time to generate a password
 
-
-
-  
-//Write password to the #password input
-
-function writePassword() {
-    var rightPrompts = getPrompts();
-    var passwordText = document.querySelector("#password");
-
-    if(rightPrompts) {
-        var newPassword = generatePassword();
-        passwordText.value = newPassword;
-    }else {
-        passwordText.value = "";
-    }
-}
-
-//this is where the password will be generated
 function generatePassword() {
-    var password = "";
-    for(var i =0; i < lenghtConfirm; i++) {
-        var randomIndex = Math.floor(Math.random() * assingChoice.lenght);
-        password = password = assignChoice[randomIndex];
-    }
-    return password;
-}
+   //users input
+    lenghtConfirm = parseInt(prompt("How many characters would you like your password to contain? (8 - 128 characters"));
 
-
-function getPrompts() {
-    assignConfirm = [];
-
-    var lenghtConfirm = (prompt("How many characters would you like your password to contain? (8 - 128 characters"));
-
-    if(isNaN(lenghtConfirm) || lenghtConfirm < 8 || lenghtConfirm > 128) {
-        alert("Password lenght has to be between 8 - 128 characters. Please Try Again!");
-        return false;
-    } 
-
-    if (confirm("Will this contain special characters?")) {
-        assignConfirm = assignConfirm.concat(specialCharacterConfirm);
-    }
-
-    if (confirm("Will this contain number characters?")) {
-        assignConfirm = assignConfirm.concat(numberCharacterConfirm);
-    }
-
-    if (confirm("Will this contain upper case characters?")) {
-        assignConfirm = assignConfirm.concat(upperCharactersConfirm);
-    }
-
-    if (confirm("Will this contain lower case characters?")) {
-        assignConfirm = assignConfirm.concat(lowerCharactersConfirm);
-    }
-    return true;
-
-}
+    if(!lenghtConfirm) {
+        alert("Please add value");
+    }else if (lenghtConfirm < 8 || lenghtConfirm > 128) {
+        //User Validation
+        //User starts input prompts
+        lenghtConfirm = parseInt(prompt("Password lenght has to be between 8-128 characters. Please try again."));
+    
+    } else {
+        // Once user is validated continue
+        specialCharactersConfirm = confirm("Will this contain special character?");
+        numberCharactersConfirm = confirm("Will this contain number characters?");
+        upperCharactersConfirm = confirm("Will this contain upper case characters?");
+        lowerCharactersConfirm = confirm("Will this contain lower case characters?");
+    };
+    
+   
